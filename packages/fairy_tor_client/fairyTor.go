3a1c9af57618ecd013a4890dcba83280598842f4
Shivergard <shivergard@greenbox.lv>
2022-11-06 23:50:47 +0200

    integrated local websocket

--- packages/fairy_tor_client/fairyTor.go
+++ packages/fairy_tor_client/fairyTor.go
@@ -10,14 +10,18 @@ import (
 	"fmt"
 	"context"
 	//"encoding/json"
+	"github.com/gorilla/websocket"
 	"os"
+	//"flag"
+	"net/http"
+	"html/template"
 )
 
 var TengoHandler telnet.Handler = internalEchoHandler{}
 
-
 type internalEchoHandler struct{}
 
+var upgrader = websocket.Upgrader{} // use default options
 
 func (handler internalEchoHandler) ServeTELNET(ctx telnet.Context, w telnet.Writer, r telnet.Reader) {
 	var fullText string
@@ -33,9 +37,9 @@ func (handler internalEchoHandler) ServeTELNET(ctx telnet.Context, w telnet.Writ
 			fullText = strings.Replace(fullText, "  ", " ", -1)
 			checkPingText = checkPingText + string(p[:n])
 
-			i := strings.Index(checkPingText, "within cells interlinked")
+			i := strings.Index(checkPingText, "fairy client ping")
 			if i > -1 {
-				fmt.Println( "!!!! Interlinked !!!!" )
+				fmt.Println( "!!!! Fairy Client !!!!" )
 				time.Sleep(30 * time.Millisecond)
 				checkPingText = ""
 
@@ -52,10 +56,10 @@ func (handler internalEchoHandler) ServeTELNET(ctx telnet.Context, w telnet.Writ
 
 				fmt.Println( "!!!! Post !!!!" )
 				// retrieve values
-				mul := compiled.Get("interlink").String()
+				mul := compiled.Get("fteftefte").String()
 
 				if mul == "" {
-					oi.LongWrite(w,[]byte("///interlink interlink"+ "\n\r"))
+					oi.LongWrite(w,[]byte("///ftefteftecnf"+ "\n\r"))
 				} else {
 					oi.LongWrite(w,[]byte(mul + "\n\r"))
 				}
@@ -82,6 +86,54 @@ func serveSimple() {
 	}
 }
 
+//var addr = flag.String("addr", "localhost:5866", "http service address")
+
+func localEcho(w http.ResponseWriter, r *http.Request) {
+	//quick fix for CORS
+	// https://github.com/gorilla/websocket/issues/367
+	upgrader.CheckOrigin = func(r *http.Request) bool { fmt.Println(r); return true }
+	
+	// c, err := upgrader.Upgrade(w, r, http.Header{
+	// 	"Host": {"interhost"},
+	// })
+
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(fmt.Sprint("upgrade:", err))
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			fmt.Println(fmt.Sprint("read:", err))
+			break
+		}
+		fmt.Println(fmt.Sprint("recv: %s", message))
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			fmt.Println(fmt.Sprint("write:", err))
+			break
+		}
+	}
+}
+
+func localHome(w http.ResponseWriter, r *http.Request) {
+ 	homeTemplate.Execute(w, "ws://"+r.Host+"/echo")
+}
+
+func localWS() {
+	//flag.Parse()
+	http.HandleFunc("/echo", localEcho)
+	http.HandleFunc("/", localHome)
+	fmt.Println( "---START WS ---")
+	// it must be localhost , not to attract attention
+	// fmt.Println(http.ListenAndServe("localhost:5866", nil))
+	// but I am testing it with Docker , so I need it public
+	fmt.Println(http.ListenAndServe("0.0.0.0:5866", nil))
+}
+var homeTemplate = template.Must(template.New("").Parse(`<html><head><title>Websocket Echo</title></head><body><script></script>{{.}}<body></html>`))
+
 func main() {
 
     err_dir := os.RemoveAll("./data-dir-*")
@@ -92,6 +144,7 @@ func main() {
 	var handler telnet.Handler = TengoHandler
 	fmt.Println( "---START TOR ---") 
 	go serveSimple();
+	go localWS();
 	err := telnet.ListenAndServe("5566", handler)
 	if nil != err {
 		//@TODO: Handle this error better.
